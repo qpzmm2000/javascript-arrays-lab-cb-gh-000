3bbfdad4768e3446afd5ddd68e0df53feade6d93
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 04:17:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-var kittens = ["Milo", "Otis", "Garfield"] 
+var kittens = ["Milo", "Otis", "Garfield"]
 
 destructivelyAppendKitten = (name) => {
   kittens.push(name)
