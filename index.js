3e68516f0b16160d657a667db96aba6461f7de52
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 04:24:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
